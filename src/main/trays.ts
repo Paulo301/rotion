b184c4de418968e62da408d4433df0112cbe0a19
Paulo301 <paulovictor1918@gmail.com>
2023-07-20 17:45:48 -0300

    Criando tray menu

---
+++ src/main/trays.ts
@@ -0,0 +1,10 @@
+import { app, Tray, Menu } from 'electron';
+import path from 'node:path';
+
+app.whenReady().then(() => {
+  const tray = new Tray(path.resolve(__dirname, 'rotionTemplate.png'));
+
+  const menu = Menu.buildFromTemplate([{ label: 'Rotion' }]);
+
+  tray.setContextMenu(menu);
+})
